92fb635a93c880c90440c930b6bb387c6c243181
Hamburgir <shantnuprofid@gmail.com>
2024-05-14 21:39:09 +0530

    Fixed money conversion rate

--- E9.java
+++ E9.java
@@ -1,36 +1,22 @@
 public class E9 {
 	private double rupee;
-
-
 	public E9(double rupee) {
 		this.rupee = rupee;
 	}
-
-
 	public double getRupee() {
 		return rupee;
 	}
-
-
 	public void setRupee(double rupee) {
 		this.rupee = rupee;
 	}
-
-
 	public double convertToDollar() {
-		// 1 Indian Rupee equals approximately 0.014 USD
-		return rupee * 0.014;
+		return rupee * 0.012;
 	}
-
-
 	public double convertToPound() {
-		// 1 Indian Rupee equals approximately 0.010 GBP
-		return rupee * 0.010;
+		return rupee * 0.0096;
 	}
-
-
 	public static void main(String[] args) {
-		E9 converter = new E9(500); // 1000 Indian Rupees
+		E9 converter = new E9(1000);
 		System.out.println("Amount in Rupee: " + converter.getRupee());
 		System.out.println("Amount in Dollar: " + converter.convertToDollar());
 		System.out.println("Amount in Pound: " + converter.convertToPound());
